fb8bed07449b8d87fdebe047d54b00e77bda4cf9
Kostiantyn Kolosov <rayleefx@protonmail.com>
2025-11-28 11:49:25 +0200

    Project created

---
+++ GridMaster/GridMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridMasterApp.swift
+//  GridMaster
+//
+//  Created by Kostiantyn Kolosov on 28.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct GridMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
